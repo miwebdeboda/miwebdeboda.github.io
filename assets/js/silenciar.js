5941f9a9d30189f7527f314bc43945fa218cce6f
Nicoguzok <109047816+Nicoguzok@users.noreply.github.com>
2023-06-29 21:34:53 -0300

    Delete silenciar.js

--- assets/js/silenciar.js
+++
@@ -1,8 +0,0 @@
-function toggleMute() {
-    var audio = document.getElementById("myAudio");
-    if (audio.muted) {
-        audio.muted = false;
-    } else {
-        audio.muted = true;
-    }
-}
