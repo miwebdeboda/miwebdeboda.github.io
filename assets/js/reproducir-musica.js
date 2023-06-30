81b97ed714eed941dde72bc1a209dd6a05a61016
Nicoguzok <109047816+Nicoguzok@users.noreply.github.com>
2023-06-29 21:35:15 -0300

    Delete reproducir-musica.js

--- assets/js/reproducir-musica.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    var audio = document.getElementById("myAudio");
-
-    audio.addEventListener('canplaythrough', function () {
-        audio.play();
-    });
-});
